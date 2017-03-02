c1dd1904757802f4eae310760dd344650d68aa17
JaFah <c.l.main@hotmail.com>
2017-03-02 16:50:04 +0000

    Basic Layout, started work on Dictionary

--- app/src/main/java/com/clmain/dissertationapp/MenuActivity.java
+++ app/src/main/java/com/clmain/dissertationapp/MenuActivity.java
@@ -1,7 +1,12 @@
 package com.clmain.dissertationapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.clmain.dissertationapp.fragments.MainMenuFragment;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.main_frame_layout, new MainMenuFragment());
+        ft.commit();
     }
+
+
 }
